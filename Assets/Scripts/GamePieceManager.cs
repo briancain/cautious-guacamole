eaa873b144c551e3d2cf30f29523120ee0f3c5b8
Brian Cain <brianccain@gmail.com>
2018-06-24 11:11:23 -0700

    Add drag and drop ability to game pieces

--- Assets/Scripts/GamePieceManager.cs
+++ Assets/Scripts/GamePieceManager.cs
@@ -14,29 +14,40 @@ public class GamePieceManager : MonoBehaviour {
   private float timer = 0f;
   private float generationCooldown = 2.0f;
 
+  private float mouseX;
+  private float mouseY;
+
   void Awake() {
     isPlaced = false;
   }
 
   // Use this for initialization
   void Start () {
+    Debug.Log("Started");
   }
   // Update is called once per frame
   void Update () {
+    mouseX = Input.mousePosition.x;
+    mouseY = Input.mousePosition.y;
   }
 
   // Pick up the piece
-  void OnMouseButtonDown() {
+  void OnMouseDown() {
+    Debug.Log("I was clicked!");
 
   }
 
   // Drop or reset the piece
-  void OnMouseButtonUp() {
+  void OnMouseUp() {
+    Debug.Log("I was let go!");
 
   }
 
   // Follow the mouse to place the piece
   void OnMouseDrag() {
+    Debug.Log("I am being dragged...");
+    transform.position = Camera.main.ScreenToWorldPoint(
+        new Vector3(mouseX,mouseY,10.0f));
 
   }
 }
