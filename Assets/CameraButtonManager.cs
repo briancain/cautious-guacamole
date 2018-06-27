30cf3e2ad5d38e08d66615b4eaf5a0559ec598e3
Brian Cain <bcain@hashicorp.com>
2018-06-27 08:06:54 -0700

    Flash camera when clicked

--- Assets/CameraButtonManager.cs
+++ Assets/CameraButtonManager.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class CameraButtonManager : MonoBehaviour {
 
+  [SerializeField]
+  Sprite camera;
+
+  [SerializeField]
+  Sprite cameraFlash;
+
+  private SpriteRenderer sr;
+
   // Use this for initialization
   void Start () {
+    sr = gameObject.GetComponent<SpriteRenderer>();
   }
   // Update is called once per frame
   void Update () {
   }
 
+
+  void OnMouseUp() {
+    sr.sprite = camera;
+  }
+
   // Photo taken!!
   void OnMouseDown() {
     GameManager gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+
+
+    sr.sprite = cameraFlash;
     gameManager.TakePhoto();
   }
 }
