c1ad22842c9d2601ff94e824e546e8cc869b1c5a
Brian Cain <brianccain@gmail.com>
2018-06-26 20:35:01 -0700

    Add background and camera button assets
    Also hook them up into the game!

--- Assets/CameraButtonManager.cs
+++ Assets/CameraButtonManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour {
+public class CameraButtonManager : MonoBehaviour {
 
   // Use this for initialization
   void Start () {
